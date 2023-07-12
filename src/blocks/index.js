a6519ca6dcb634c2372d708c189f725ede62e862
Alexander Guzyaev <avguzyaev@1cbit.ru>
2023-07-12 22:18:31 +0300

    feature-000001: Сделали трехмерную болванку для сайта

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,1 +1,5 @@
 console.log('blocks');
+//= logo/_logo.js
+//= layer/_layer.js
+//= hero/_hero.js
+//= rain/_rain.js
---
+++ src/blocks/layer/_layer.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const xRontationCenter = (x) => {
+	return x - window.innerWidth / 2;
+}
+
+const yRontationCenter = (y) => {
+	return y - window.innerHeight / 2;
+}
+
+document.addEventListener('mousemove', event => {
+	const sensivityX = -0.009;
+	const sensivityY = -0.002;
+	Object.assign(document.documentElement, {
+		style: `--move-x: ${yRontationCenter(event.clientY) * sensivityX}deg; --move-y: ${xRontationCenter(event.clientX) * sensivityY}deg`
+	})
+});
